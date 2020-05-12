bf5ca38d5ca1d6cfca04c2b45ac0bb049bd4ff94
LuffyAkai <LuffyAkai@users.noreply.github.com>
2020-05-12 18:12:46 -0500

    Simplify

---
+++ include/NodBody.h
@@ -0,0 +1,25 @@
+#ifndef BODY_H
+#define BODY_H
+
+#include "NodMath.h"
+
+struct Body
+{
+public:
+	//Constructores
+	Body() {}
+
+	//Mostrar información del cuerpo
+	void DisplayInfo();
+
+	//Aplicación de fuerzas
+	void addForce(const Vec2& f);
+
+	float mass;
+	float invMass = 1 / mass;
+	Vec2 position;
+	Vec2 velocity;
+	Vec2 force = Vec2(0.0f, 0.0f); //La mejoraremos eventualmente. 
+};
+
+#endif
